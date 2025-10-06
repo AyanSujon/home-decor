6c86513598d8a2cf8c624c64b67e8ab2a4ec8b42
Ayan <sujon.seo.bd@gmail.com>
2025-10-06 13:08:05 +0600

    initialized project

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router";
+import Home from "../Pages/Home";
+import Products from "../Pages/Products";
+import MainLayout from "../Layouts/MainLayout";
+import ErrorPage from "../Pages/ErrorPage";
+import Wishlist from "../Pages/Wishlist";
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout/>,
+    errorElement: <ErrorPage/>,
+    children: [
+        {
+            index: true,
+            element: <Home/>,
+        },
+        {
+        path: "/products",
+        element: <Products/>,
+        },
+        {
+        path: "/wishlist",
+        element: <Wishlist/>,
+        },
+    ]
+  },
+
+
+]);
+export default router
