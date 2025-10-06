47080ee3e97306cee0c63cc3e2c2e9eac56ab151
Ayan <sujon.seo.bd@gmail.com>
2025-10-06 13:12:55 +0600

    Facth data in home page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -17,6 +17,7 @@ const router = createBrowserRouter([
         {
             index: true,
             element: <Home/>,
+            loader: ()=> fetch("/furnitureData.json"),
         },
         {
         path: "/products",
